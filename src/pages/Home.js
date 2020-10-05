0e6f62e3a4f8631a33577cb2519ea17a7a3813ae
MarKoRn <mark7dev@hotmail.com>
2020-10-05 14:47:22 -0500

    add pagination

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,12 +7,31 @@ import Card from '../Components/Card';
 const Home = () => {
 
     const [shows, setShows] = useState([]);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [isSearch, setIsSearch] = useState(false);
+    const [showFound, setShowFound] = useState({});
 
-    const getShows = () => {
-        axios.get('http://api.tvmaze.com/shows?page=1')
+    const nextPage = () => {
+        let next = currentPage + 1;
+        setCurrentPage(next);
+    }
+
+    const previousPage = () => {
+        let previuos = currentPage - 1;
+        setCurrentPage(previuos);
+    }
+
+    const getData = () => {
+        // let getShows = 'http://api.tvmaze.com/shows?page=203';
+        let getShows = `http://api.tvmaze.com/shows?page=${currentPage}`;
+        let getSearch = 'http://api.tvmaze.com/singlesearch/shows?q=girls';
+        let url
+        isSearch ? url = getSearch : url = getShows;
+        // axios.get(`http://api.tvmaze.com/shows?page=${currentPage}`)
+        axios.get(url)
         .then(response => {
             console.log(response.data);
-            setShows(response.data);
+            isSearch ? setShowFound(response.data) : setShows(response.data);
         })
         .catch(error => {
             console.log(error);
@@ -20,12 +39,22 @@ const Home = () => {
     };
 
     useEffect(() => {
-        getShows();
-    }, [])
+        getData();
+    }, [currentPage]);
 
     return ( 
         <div>
             <Search />
+            <p>{currentPage}</p>
+            <button
+                onClick={previousPage}
+            >Last</button>
+            <button
+                onClick={nextPage}
+            >Next</button>
+            <Card 
+                show={showFound}
+            />
             <List 
                 shows={shows}
             />
